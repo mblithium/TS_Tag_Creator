654a51f07044f2b7ddc62780ed97df4a8645017f
Matheus Bastos <matheusmcy1@gmail.com>
2023-08-18 14:10:16 -0300

    Initial dev release

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TS Tag Creator
+
+A tool to create timestamps, where it saves in a json file or another format. Timestamps consist of start time and end time.
+
+It's still in development and a lot can change.
+
+# How to use
+
+- Click **"CHOOSE FILE"** and select a video on your device, it will be loaded on the page. 
+- Here you will have the player option normally, you will have to press **"Add"** to add a timestamp. The program will automatically organize by time, the first being the start time and the second the end time.
+
+You can also clear all timestamps by clicking **"clear"**.
+
+# License 
+
+[GNU GENERAL PUBLIC LICENSE V3](./LICENSE)
+
---
+++ public/scripts/script.js
@@ -0,0 +1,111 @@
+const videobtn = document.querySelector("#video");
+const createTmpbtn = document.querySelector("#createTimestamp");
+const clearTmpbtn = document.querySelector("#clearTimestamp");
+const viewer = document.querySelector("#viewer");
+const videosource = document.createElement("video");
+const timestampsArea = document.querySelector("#timestamps");
+
+let timestamps_Data = {};
+
+
+URL = window.URL || window.webkitURL;
+
+videobtn.addEventListener("change", (e) => {
+    let ev_file = e.target.files[0];
+    let vd = createVideoBlobURL(ev_file);
+
+    // Reset
+    timestamps_Data = {};
+    timestamps_Data.timestamps = [];
+    timestamps_Data.filename = e.target.value.slice(12);
+
+    timestampsArea.innerHTML = "";
+
+    changeVideoView(vd);
+});
+
+function changeVideoView(url) {
+    videosource.src = url;
+    videosource.canPlayType = "video/mp4";
+    videosource.controls = true;
+
+    viewer.innerHTML = "";
+    viewer.append(videosource);
+}
+
+function createVideoBlobURL(blob) {
+    let url = URL.createObjectURL(blob);
+    return url;
+}
+
+function saveTimestamps() {
+    // TODO: Faz o download dos timestamps para um arquivo de text.
+    
+}
+
+// TODO: Arrumar evento de clear não funcionando.
+
+function clearTimestamps() {
+    timestamps_Data.timestamps = [];
+    renderTimestamps();
+}
+
+clearTmpbtn.addEventListener("click", (e) => { clearTimestamps(); });
+
+function renderTimestamps() {
+    timestampsArea.innerHTML = "";
+
+    for (i in timestamps_Data.timestamps) {
+        let span = document.createElement("span");
+        span.innerHTML = timestamps_Data.timestamps[i];
+
+        if (i % 2 == 0) { 
+            span.className = "tspStart"; 
+            span.title = "start time";
+        } 
+        else { 
+            span.className = "tspEnd";
+            span.title = "end time"; 
+        }
+
+        timestampsArea.appendChild(span);
+    }
+
+    let spanref = document.querySelectorAll("#timestamps > span");
+
+    if (spanref.length > 0) {
+        spanref.forEach(element => {
+            element.addEventListener("click", (e) => { 
+                handleVideo("currentTime", e.target.innerHTML);
+            });
+        });
+    };
+    
+}
+
+function handleVideo(option, value) {
+    let viewer_video = document.querySelector("video");
+
+    if (option == "currentTime") { 
+        //viewer_video.pause();
+        viewer_video.currentTime = value; 
+    }
+}
+
+createTmpbtn.addEventListener("click", (e) => { createTimestamp(); });
+
+function createTimestamp() {
+    let viewer_video = document.querySelector("video");
+    let currentTime = viewer_video.currentTime;
+    let tspdata = timestamps_Data.timestamps;
+
+    viewer_video.pause();
+
+    if (!tspdata.includes(currentTime)) {
+        timestamps_Data.timestamps.push(currentTime);
+    }
+
+    timestamps_Data.timestamps.sort((a, b) => { return a - b; })
+    renderTimestamps();
+}
+
